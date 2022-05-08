b3ef6a4c97ba20913cd779081afa37b6e6af96a5
RafiUlI <ctgrafi150@gmail.com>
2022-05-08 15:20:01 +0600

    get one inventory

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ async function run() {
             const inventories = await cursor.toArray()
             res.send(inventories);
         })
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const inventory = await itemCollection.findOne(query);
+            res.send(inventory);
+        })
     }
     finally {
 
